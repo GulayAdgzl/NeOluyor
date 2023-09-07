4e1a698756d44c547f3eb475c78e8aca88b382a9
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-09-07 23:56:23 +0300

    ApiServise GET HTTP add

--- app/src/main/java/com/example/neoluyor/api/ApiService.kt
+++ app/src/main/java/com/example/neoluyor/api/ApiService.kt
@@ -1,4 +1,13 @@
 package com.example.neoluyor.api
 
+import com.example.neoluyor.response.HaberResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+    //https://newsapi.org/v2/top-headlines?country=tr&apiKey=cc89e561914e4e33a8f45aa7adcb03eb
+
+    @GET("top-headlines?country=tr")
+    fun getNewsPaper(@Query("page") page:Int): Call<HaberResponse>
 }
